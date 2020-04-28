c984f7d5d3d73b4b616e004b0c4339784ecaef82
Yuta Saito <kateinoigakukun@gmail.com>
2020-04-28 18:22:28 +0900

    Proxy stdout and stderr

--- src/index.js
+++ src/index.js
@@ -3,22 +3,34 @@ import { WASI } from "@wasmer/wasi";
 import { WasmFs } from "@wasmer/wasmfs";
 
 
-const swift = new SwiftRuntime();
-const wasmFs = new WasmFs();
-let wasi = new WASI({
-  args: [],
-  env: {},
-  bindings: {
-    ...WASI.defaultBindings,
-    fs: wasmFs.fs
-  }
-});
-
-wasmFs.fs.createReadStream('/dev/stderr', 'utf8').on('data', data => {
-  console.error(data.toString());
-})
-
 const startWasiTask = async () => {
+
+  const swift = new SwiftRuntime();
+  const wasmFs = new WasmFs();
+
+  // Output stdout and stderr to console
+  const originalWriteSync = wasmFs.fs.writeSync;
+  wasmFs.fs.writeSync = (fd, buffer, offset, length, position) => {
+    const text = new TextDecoder("utf-8").decode(buffer);
+    switch (fd) {
+    case 1:
+      console.log(text);
+      break;
+    case 2:
+      console.error(text);
+      break;
+    }
+    return originalWriteSync(fd, buffer, offset, length, position);
+  };
+
+  let wasi = new WASI({
+    args: [], env: {},
+    bindings: {
+      ...WASI.defaultBindings,
+      fs: wasmFs.fs
+    }
+  });
+
   const response = await fetch("LifeGameWeb.wasm");
   const importObject = {
     wasi_snapshot_preview1: wasi.wasiImport,
@@ -30,6 +42,5 @@ const startWasiTask = async () => {
   swift.setInsance(instance);
   wasi.start(instance);
 };
-startWasiTask().catch(err => {
-  console.error(err);
-});
+
+startWasiTask().catch(console.error);
