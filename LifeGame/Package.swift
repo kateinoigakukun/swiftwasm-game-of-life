03baad94d875e438d0d6314c322c8ed2399ff9b1
Yuta Saito <kateinoigakukun@gmail.com>
2020-10-26 14:07:16 +0900

    Add executable target to use carton

--- LifeGame/Package.swift
+++ LifeGame/Package.swift
@@ -5,9 +5,9 @@ import PackageDescription
 let package = Package(
     name: "LifeGame",
     products: [
-        .library(
-            name: "LifeGame",
-            targets: ["LifeGame"]),
+        .executable(
+            name: "LifeGameWeb",
+            targets: ["LifeGameWeb"]),
     ],
     dependencies: [
         .package(
